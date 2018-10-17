449fc2cc9149944bf41ef55093b62dbeb7651ffb
annabriet <abjarkadottir@gmail.com>
2018-10-17 11:56:54 +0000

    P1 b

---
+++ P1.b/sketch.js
@@ -0,0 +1,12 @@
+// Hér kemur kóðinn þinn:
+function setup(){
+  createCanvas(564,735);
+  background(235,214,167);
+  stroke(105,90,31)
+  line(300, 0, 300, 192);
+  noFill();
+  curve(4, -200 ,200, 5, 400, 5, 600, -200);
+  line(200,5,200,70)
+  line(400, 5, 400, 140)
+  line(100,70,300,70)
+}
