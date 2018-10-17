455d8ed7e912fb3d54304d6b2f0dfb3946ee1f8a
annabriet <abjarkadottir@gmail.com>
2018-10-17 09:51:59 +0000

    nýr hringur+kassi

--- P0-HalloHeimur/sketch.js
+++ P0-HalloHeimur/sketch.js
@@ -4,9 +4,9 @@ function setup(){
     background('rgba(0,255,0, 0.25)');
     fill('#fae');
     ellipse(300,300,50,50);
-    fill(0);
+    fill('rgba(100%,0%,100%,0.5)');
     textSize(35);
     text("Halló heimur",100,50);
+    rect(200,200,10,10)
+    ellipse(100,200,200,200);
 }
-    rect(200,200,10)
-    ellise(100,200,50,50);
