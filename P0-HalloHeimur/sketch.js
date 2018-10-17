f4419809d13e8a426100776fa3e2bb489e15af0d
annabriet <abjarkadottir@gmail.com>
2018-10-17 09:37:40 +0000

    veit ekki virkar ekki

--- P0-HalloHeimur/sketch.js
+++ P0-HalloHeimur/sketch.js
@@ -1,11 +1,12 @@
 // Hér kemur kóðinn þinn:
 function setup(){
     createCanvas(400,400);
-    background('rgb(0,255,0)');
+    background('rgba(0,255,0, 0.25)');
     fill('#fae');
     ellipse(300,300,50,50);
     fill(0);
     textSize(20);
     text("Halló heimur",200,40);
 }
-  ellipse(10,10,70,70)
+    rect(200,200,10)
+    ellise(100,200,50,50);
