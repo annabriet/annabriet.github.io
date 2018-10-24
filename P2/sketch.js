8228f31bf0daff30242cad8a0e26e513215e1b91
annabriet <abjarkadottir@gmail.com>
2018-10-24 08:28:01 +0000

    P2

---
+++ P2/sketch.js
@@ -0,0 +1,14 @@
+// Hér kemur kóðinn þinn:
+var eyeSize = 20;
+var faceWidth = 100;
+var faceHeight = 150;
+var x = 150;
+var y = 150;
+
+function setup(){
+  createCanvas (300,300);
+}
+
+function draw(){
+  background(180);
+  ellipse(x, y, faceWidth, faceHeight);
