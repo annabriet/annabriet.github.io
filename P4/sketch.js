d9cb58ab93edee79370ed9d7b1a51931db71cbfc
annabriet <abjarkadottir@gmail.com>
2018-11-05 12:21:03 +0000

    P4

--- P4/sketch.js
+++ P4/sketch.js
@@ -1,12 +1,26 @@
 // Hér kemur kóðinn þinn:
 function setup() {
-  createCanvas(400,300);
+  createCanvas(500,500);
   background(255,200,0);
-  fill(236,36,94);
+
+  rectMode(CENTER)
 }
 
 function draw(){
-  if(mouseX < 200){
-    ellipse(mouseX, mouseY, 30, 30);
-  }
+  fill(236,36,94);
+  if((mouseX < 235) && (mouseY < 235)){
+  rect(mouseX, mouseY, 50, 50);
 }
+  if((mouseX >265)&&(mouseY <235)){
+  fill(200,191,231)
+  ellipse(mouseX, mouseY, 30, 30)
+}
+ if((mouseX <265)&&(mouseY > 265)){
+ fill(128,255,128)
+ ellipse(mouseX, mouseY, 30,30)
+}
+ if((mouseX>265)&&(mouseY>265)){
+   fill(255,128,255)
+   rect(mouseX, mouseY, 50,50)
+ }
+ }
