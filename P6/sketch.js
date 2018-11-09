ff8d56b6de06f5b5d232d2fdfb57e32ccff7b636
annabriet <abjarkadottir@gmail.com>
2018-11-09 13:49:48 +0000

    P6 áfram

--- P6/sketch.js
+++ P6/sketch.js
@@ -1,42 +1,46 @@
 // Hér kemur kóðinn þinn:
 // x og xSpeed eru breytur (variables).
-    // x byrjar í 0 (vinstra megin á skjánum) en færist um 5 í hverju skrefi
-    var y = 0;
-    var ySpeed = 5;
-    var x=0
-    var xSpeed= 5
-    var spaðiY
-    var boltistærð = 20
-    var spaðiþykkt=20
-    var spaðibreidd = 70
+// x byrjar í 0 (vinstra megin á skjánum) en færist um 5 í hverju skrefi
+var x=100;
+var y = 100;
+var ySpeed = 7;
+var xSpeed = 5;
+var spaðiY;
+var boltistærð = 20;
+var spaðiþykkt = 20;
+var spaðibreidd = 100;
+var stig = 0;
 
+function setup() {
+  createCanvas(700,600);
+  spaðiY = height-50;
+  rectMode(CENTER);
+  textFont("Courier New",16);
+  textStyle(BOLD);
+}
 
-    function setup() {
-      createCanvas(400,300);
-      spaðiY = height-50
-      rectMode(CENTER)
+function draw() {
+  background(255,200,0);
+  fill(236,36,94);
+  ellipse(x, y, boltistærð, boltistærð);
+  fill(0,0,0);
+  rect(mouseX,spaðiY,spaðibreidd,spaðiþykkt);
+  fill(0);
+  text("stig: " + stig,20,20);
+
+    // Reikna ný hnit út frá hraða boltans:
+    x = x + xSpeed;
+    y = y + ySpeed;
+    // Athuga hvort boltinn snertir veggi:
+    if ((x > width-boltistærð/2) || (x < boltistærð/2)){
+      xSpeed = xSpeed * -1;
     }
-
-    function draw() {
-      background(255,200,0);
-      fill(236,36,94);
-      ellipse(150, y, 20,20);
-      fill(0,0,0)
-      rect(mouseX,spaðiY,spaðibreidd,spaðiþykkt)
-      y = y + ySpeed;
-        //ef boltinn er kominn út á enda niðri á hann að snúa við
-      if(y > height){
-        ySpeed = -1*ySpeed;
-      }
-      //ef boltinn er kominn út á enda uppi á hann líka að snúa við!
-      if(y < 0||y>height){
-      if(x < 0|| x>width)
-        ySpeed = -1*ySpeed;
-      }
-      if (y > (height-15)) {
-      ySpeed = ySpeed * -0.9;
-      y = height-15;
+    if ((y > height-boltistærð/2) || (y < boltistærð)) {
+      ySpeed = ySpeed * -1;
     }
-
-
+    //Athuga hvort bolti snerti spaða
+    if(abs(spaðiY-y) < boltistærð/2 + spaðiþykkt && abs(mouseX-x) < boltistærð/2 +spaðibreidd/2){
+      ySpeed = ySpeed* -1;
+      stig= stig+1
     }
+  }
